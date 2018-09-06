e91e6cc5ed6fdcc051018a1bf9d284cc89826676
Medit <medit.ding@shareworks.cn>
2018-09-06 14:58:22 +0800

    common commit.

---
+++ Absorb.ProtoActor.HelloWorld/Program.cs
@@ -0,0 +1,44 @@
+﻿using Proto;
+using System;
+using System.Threading.Tasks;
+
+namespace Absorb.ProtoActor.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var props = Actor.FromProducer(() => new HelloActor());
+            var pid = Actor.Spawn(props);
+
+            pid.Tell(new Hello { Who = "Medit Ding" });
+
+            //Console.ReadLine();
+
+            pid.Stop();
+
+            Console.ReadKey();
+        }
+    }
+    class Hello
+    {
+        public string Who;
+    }
+    class HelloActor : IActor
+    {
+        public Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case Started started:
+                    Console.WriteLine("Started");
+                    break;
+                case Hello hello:
+                    Console.WriteLine($"Hello {hello.Who}");
+                    break;  
+            }
+            return Actor.Done;
+        }
+    }
+}
+
