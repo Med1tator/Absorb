d34533c26b6bfda05203f6c638fbb43d05c3425e
Medit <medit.ding@shareworks.cn>
2018-09-12 18:06:20 +0800

    commit.

---
+++ Absorb.DotNetCore.UtilityLibraries/StringLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Absorb.DotNetCore.UtilityLibraries
+{
+    public static class StringLibrary
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            return char.IsUpper(str[0]);
+        }
+
+        public static bool StartsWithLower(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            return char.IsLower(str[0]);
+        }
+    }
+}
