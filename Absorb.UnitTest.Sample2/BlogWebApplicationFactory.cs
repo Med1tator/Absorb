19267ddcb7b026d3224c8403e4d5701b7df3a02e
Medit <medit.ding@shareworks.cn>
2018-10-23 11:39:12 +0800

    commit.

---
+++ Absorb.UnitTest.Sample2/BlogWebApplicationFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Absorb.UnitTest.Sample2
+{
+    public class BlogWebApplicationFactory<TStartup>:WebApplicationFactory<TStartup> where TStartup:class
+    {
+    }
+}
---
+++ Absorb.UnitTest.Sample2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Absorb.UnitTest.Sample2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
